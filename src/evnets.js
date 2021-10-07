e0e93954d0b4f88101a37c446a70fd2eaf565502
이유성 <82145837+meteor-or@users.noreply.github.com>
2021-10-07 17:45:49 +0900

    Feture: BigCalendar  라이브러리 구현

---
+++ src/evnets.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: "온살",
+    allDay: true,
+    start: new Date(2021, 9, 6),
+    end: new Date(2021, 9, 6),
+    onRangeChange:
+  },
+  {
+    title: "제압",
+    start: new Date(2021, 9, 7),
+    end: new Date(2021, 9, 7),
+  },
+
+  {
+    title: "봉바",
+    start: new Date(2021, 9, 8),
+    end: new Date(2021, 9, 8),
+  },
+
+  {
+    title: "쇄빙",
+    start: new Date(2021, 9, 9),
+    end: new Date(2021, 9, 9),
+  },
+];
