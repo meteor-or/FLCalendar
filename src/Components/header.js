d5be2c9d109f4559d0b2ff5fb75116ba4b63034b
이유성 <82145837+meteor-or@users.noreply.github.com>
2021-10-23 19:38:18 +0900

    Style: general page style

--- src/Components/header.js
+++ src/Components/header.js
@@ -5,7 +5,7 @@ function Header() {
     <>
       <div className="top-bar">
         <h1 className="logo">
-          <a href="#">오늘 전장</a>
+          <a href="#">DailyFL</a>
         </h1>
       </div>
     </>
