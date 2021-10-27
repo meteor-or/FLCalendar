a1e63db72a261cf1c53f56b4bc43da007a889597
이유성 <82145837+meteor-or@users.noreply.github.com>
2021-10-27 15:49:46 +0900

    Feat:shatter tooltip timetable
    쇄빙 얼음 시간표 추가

--- src/Components/FLcard.js
+++ src/Components/FLcard.js
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react";
 import "./FLcard.css";
-import { Button, Overlay, Tooltip, OverlayTrigger } from "react-bootstrap";
+import { Button, Tooltip, OverlayTrigger } from "react-bootstrap";
 
 function FLcard(props) {
   const [show, setShow] = useState(false);
@@ -41,23 +41,55 @@ function FLcard(props) {
             <h4 className="pattern_title heading">
               <span>패턴</span>
             </h4>
+            <div className="descOfPattern">{props.data.pattern1}</div>
             <div className="descOfPattern">
-              {props.data.pattern1}
-              {/* {["top"].map((placement) => (
-                <OverlayTrigger
-                  key={placement}
-                  placement={placement}
-                  overlay={
-                    <Tooltip id={`tooltip-${placement}`}>
-                      Tooltip on <strong>{placement}</strong>.
-                    </Tooltip>
-                  }
-                >
-                  <Button variant="secondary">?</Button>
-                </OverlayTrigger>
-              ))} */}
+              {props.data.title == "영광의 평원(쇄빙전)" && (
+                <>
+                  {["top"].map((placement) => (
+                    <OverlayTrigger
+                      key={placement}
+                      placement={placement}
+                      overlay={
+                        <Tooltip id={`tooltip-${placement}`}>
+                          ┏ 　I　II　III　 ┳ TIME ┳ I　 II　 III　IV ┓
+                          <br />
+                          ┃ 　　　　　　┃19 : 45┃ 1　 6　 1　6 ┃ <br />
+                          ┃ 　1　1　1　 ┃19 : 00┃ 1　 1　 1　　┃
+                          <br />
+                          ┃ 　　　　　　┃18 : 00┃ 1　 1　 1　　┃
+                          <br />
+                          ┃ 　　　　　　┃17 : 00┃ 1　 1　 1　　┃
+                          <br />
+                          ┃ 　1 　　　　┃16 : 00┃ 1　 1　 4　4 ┃
+                          <br />
+                          ┃ 　　 1　2 　┃15 : 00┃ 1　 1　 　 　┃
+                          <br />
+                          ┃ 　　　　　　┃14 : 00┃ 1　 1　 　 　┃
+                          <br />
+                          ┃ 　　　　　　┃13 : 00┃ 1　 1　 　 　┃
+                          <br />
+                          ┃ 　1 　　　　┃12 : 00┃ 1　 1　 4　4 ┃
+                          <br />
+                          ┃ 　　　　　　┃11 : 00┃ 1　 1　 　 　┃
+                          <br />
+                          ┃ 　　 2　1 　┃10 : 00┃ 1　 1　 　 　┃
+                          <br />
+                          ┃ 　　　　　　┃09 : 00┃ 1　 1　 　 　┃
+                          <br />
+                          ┃ 　1 　　　　┃08 : 00┃ 6　 1　 6　4 ┃
+                          <br />┗ 　<em>큰 얼음</em> 　┻ ━━━ ┻　{" "}
+                          <em>작은얼음</em>
+                          　┛
+                        </Tooltip>
+                      }
+                    >
+                      <Button variant="secondary">시간표 보기</Button>
+                    </OverlayTrigger>
+                  ))}
+                </>
+              )}
+              {props.data.pattern2}
             </div>
-            <div className="descOfPattern">{props.data.pattern2}</div>
           </div>
         </div>
       </div>
